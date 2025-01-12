5099358192cf2161631d2383d37c8b376ad83e74
JadynF <jrf038@email.latech.edu>
2025-01-12 13:54:53 -0600

    Added backend static hosting, updated landing page

--- dbmeister/src/pages/Landing.js
+++ dbmeister/src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Landing.css"
+import "./Landing.css";
+import Collaboration from "../resources/Collaboration.jpg";
 
 function Landing() {
     return (
@@ -14,7 +15,22 @@ function Landing() {
                 </div>
             </header>
             <div className = "landing-body">
-                Body
+                <div className = "body-centered-main">
+                    <div className = "inner-text">
+                        <h1>Create Free Online Schema Diagrams!</h1>
+                        <p>Speed up production and boost understanding in your project by creating a detailed schema. DBMeister allows you to show not only internal data flow, but external as well. The ultimate tool to minimize confusion in your database project.</p>
+                        <button className = "header-signup">Create Diagram!</button>
+                    </div>
+                </div>
+                <div className = "body-full">
+                    <div className = "collab-image">
+                        k
+                    </div>
+                    <div className = "collab-text">
+                        <h1>Increased Collaboration</h1>
+                        <h3>DBMeister nurtures collaboration within a development team. Groups can be created within DBMeister so that developer teams can seamlessly work together on the same diagram. Members of the group can interact with each other with a group chat, group priviledge management, real-time diagram changes, and more!</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql2/promise');
+const path = require('path');
+
+app.use(cors());
+app.use(express.json());
+
+// SQL DATABASE CONNECTION
+// Create a MySQL connection pool
+//const pool = mysql.createPool({
+//    host:,
+//    user:,
+//    password:,
+//    database:,
+//    port:
+//});
+
+//Sends promised SQL queries
+async function sendQuery(query, list=[]) {
+    try {
+        const queryResponse = await pool.query(query, list);
+        return queryResponse;
+    }
+    catch (error) {
+        console.log("Database Error: ", error);
+        return false;
+    }
+}
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(8000, () => {
+    console.log(`Server listening on 8000`);
+});

9face3af85d30a0ce561411b4d7bcd1a40bb5851
JadynF <jrf038@email.latech.edu>
2025-04-14 23:22:18 -0500

    projectEditor fix

--- app/projectEditor/[id]/page.tsx
+++ app/projectEditor/[id]/page.tsx
@@ -676,7 +676,7 @@ export default function Project() {
                                         </div>
                                     </HoverCardTrigger>
                                     <HoverCardContent>
-                                        <div>
+                                        <div key={user}>
                                             {user}
                                         </div>
                                     </HoverCardContent>

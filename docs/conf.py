653fde0d2d482bd3e8b148a6c1f61af1c8b501e3
JadynF <jrf038@email.latech.edu>
2025-04-29 21:02:08 -0500

    doc changes

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,13 @@ release = '1.0.0'
 
 extensions = ['recommonmark']
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 

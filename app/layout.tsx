ed6db55158cee8eafc5a93828112461df37ef5c4
ratianiluka <lra013@latech.edu>
2025-05-05 15:24:37 -0500

    Changed the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   description: "A powerful database management tool with intuitive interfaces",
   viewport: "width=device-width, initial-scale=1",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/DBMEISTER-LOGO.png",
   },
 };
 
